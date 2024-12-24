648599542f6786b46158adf3818cda9ba5a19315
Hasan Ali <152736148+H-Ali13381@users.noreply.github.com>
2024-12-23 19:41:20 -0500

    Add files via upload

---
+++ ResNet50-api.py
@@ -0,0 +1,57 @@
+# 2220426 - Hasan Ali 
+
+from fastapi import FastAPI, UploadFile, File
+import uvicorn
+from tensorflow.keras.applications import ResNet50
+from tensorflow.keras.applications.resnet50 import preprocess_input, decode_predictions
+import numpy as np
+from PIL import Image
+import io
+
+
+app = FastAPI(title="ResNet50 Classifier")
+
+#Importing model
+model = ResNet50(weights='imagenet')
+
+@app.get("/")
+def read_root():
+    return {"Working": "true"}
+
+
+    
+@app.post("/predict")
+async def classify(image_file: UploadFile = File(...)):
+    """
+    Classifies an image
+    """
+
+    image = await image_file.read()
+    
+    # Decode image and preprocess
+    img = Image.open(io.BytesIO(image))#.convert('RGB')
+    img_resized = img.resize((224, 224))
+    img_array = np.array(img_resized)
+    img_array = np.expand_dims(img_array, axis=0)
+    
+    # Preprocessing image
+    preprocessed_image = preprocess_input(img_array)
+    
+    # Making a prediction
+    predictions = model.predict(preprocessed_image)
+    
+    # Decode the predictions
+    decoded_predictions = decode_predictions(predictions, top=1)[0][0]
+    
+    predicted_class = str(decoded_predictions[1])
+    certainty = f'{float(decoded_predictions[2])*100:.2f}%' 
+    
+    return {'Predicted class': predicted_class,
+            'Certainty:': certainty}
+    
+    
+if __name__ == "__main__":
+    uvicorn.run("ResNet50-api:app",
+                host='127.0.0.1',
+                port=8000,
+                reload=True)
---
+++ ResNet50-streamlit.py
@@ -0,0 +1,33 @@
+# 2220426 - Hasan Ali 
+
+import streamlit as st
+import requests
+
+def main():
+    st.title("ResNet50 Image Classifier", anchor=False)
+    
+    uploaded_file = st.file_uploader('Choose an image to classify:','jpg')
+    
+    run = st.button('Classify!')
+    
+    if run and uploaded_file is not None:
+        
+        with st.spinner('Working...'):
+
+            url = "http://127.0.0.1:8000/predict"
+            
+            
+            # This is needed to format the data files- alternatively, can use Request type in api for binary.
+            files = {'image_file': ('filename.jpg', uploaded_file, 'image/jpeg')} 
+            
+            
+            api = requests.post(url, files=files)
+            result = api.json()
+            
+            st.write(result)
+            
+            #st.subheader(f"Classification: {result['Predicted class']}")
+            
+
+if __name__ == "__main__":
+    main()
