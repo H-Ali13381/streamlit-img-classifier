0da5980469e6ce0416e3e8460ac77f0455b3f165
Hasan Ali <152736148+H-Ali13381@users.noreply.github.com>
2024-12-23 22:23:27 -0500

    Update ResNet50-streamlit.py

--- ResNet50-streamlit.py
+++ ResNet50-streamlit.py
@@ -14,7 +14,8 @@ def main():
         
         with st.spinner('Working...'):
 
-            url = "http://127.0.0.1:8000/predict"
+            #url = "http://127.0.0.1:8000/predict" --- for local deploy
+            url = https://streamlit-img-classifier-backend.onrender.com/ # --- hosted location for api
             
             
             # This is needed to format the data files- alternatively, can use Request type in api for binary.
